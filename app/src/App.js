b47228278c30cad7f92fb0e0e87f8dfd11e650a4
sbcodes90 <sydneycburrell29@gmail.com>
2024-07-28 22:36:13 -0400

    React Router config and setup with landing page

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import './App.css';
-import logo from "./images/new-header.png"
+import LandingPage from "./components/LandingPage";
+
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-teal-200 min-h-screen">
-    <img src={logo} alt="logo" />
-    <button className="bg-yellow-400 px-16 py-4 lg:py-6 rounded-3xl font-black -translate-y-7 lg:-translate-y-12 text-xs lg:text-lg">Enter the hive</button>
-  </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+   
 
   );
 }
---
+++ app/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../images/new-header.png";
+
+function LandingPage() {
+  return (
+    <div className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-teal-200 min-h-screen">
+    <img src={logo} alt="logo" />
+    <button className="bg-yellow-400 px-16 py-4 lg:py-6 rounded-3xl font-black -translate-y-7 lg:-translate-y-12 text-xs lg:text-lg">Enter the hive</button>
+  </div>
+  )
+}
+
+export default LandingPage
