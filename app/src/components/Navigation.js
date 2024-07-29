ab875b81d97c95a19ec79134fec3515b81eb90b6
sbcodes90 <sydneycburrell29@gmail.com>
2024-07-29 10:01:39 -0400

    added bee to nav

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
+import newBee from "../images/new-bee.png";
 
 export default function Navigation () {
 
@@ -10,7 +11,8 @@ export default function Navigation () {
     <div>
       <nav className="flex items-center justify-between flex-wrap bg-black p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
+          <img src={newBee} alt="bee-logo" className='w-10 ml-5'/>
+         {/*  <svg
             className="fill-current h-8 w-8 mr-2"
             width="54"
             height="54"
@@ -18,7 +20,7 @@ export default function Navigation () {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
+          </svg> */}
           <span className="font-regular text-xl tracking-tight">
             <Link to="/home/:welcome">123AllEyesOnMe LLC</Link>
           </span>
