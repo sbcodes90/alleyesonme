ebc4da59cb26b3ccfccdf62d23cbd19c0bda48ba
sbcodes90 <sydneycburrell29@gmail.com>
2024-07-28 22:45:09 -0400

    Add Navigation and outlet

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element={<Home />}>
+      <Route path="about" />
+      </Route>
     </Routes>
    
 
--- app/src/components/LandingPage.js
+++ app/src/components/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from "../images/new-header.png";
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
     <div className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-teal-200 min-h-screen">
     <img src={logo} alt="logo" />
-    <button className="bg-yellow-400 px-16 py-4 lg:py-6 rounded-3xl font-black -translate-y-7 lg:-translate-y-12 text-xs lg:text-lg">Enter the hive</button>
+    <Link to="/home"><button className="bg-yellow-400 px-16 py-4 lg:py-6 rounded-3xl font-black -translate-y-7 lg:-translate-y-12 text-xs lg:text-lg">Enter the hive</button></Link>
   </div>
   )
 }
