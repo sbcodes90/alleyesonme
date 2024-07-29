809d9db49f229631d5a1814467f56a89ccc865d1
sbcodes90 <sydneycburrell29@gmail.com>
2024-07-28 22:14:07 -0400

    landing page

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-
+import logo from "./images/new-header.png"
 function App() {
   return (
-    <div className="App">
-       <div className="bg-blue-500 text-white p-4">
-      This is a Tailwind CSS styled component.
-    </div>
-    </div>
+    <div className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-teal-200 min-h-screen">
+    <img src={logo} alt="logo" />
+    <button className="bg-yellow-400 px-16 py-5 rounded-3xl font-black -translate-y-10">Enter the hive</button>
+  </div>
+
   );
 }
 
