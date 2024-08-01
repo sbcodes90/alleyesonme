787769cf062b94d7b8cc81df4a241a2f6e8accc6
sbcodes90 <sydneycburrell29@gmail.com>
2024-08-01 17:35:03 -0400

    Program Info Page Refactoring

--- app/src/components/ProgramInfo.js
+++ app/src/components/ProgramInfo.js
@@ -1,56 +1,61 @@
-import React from 'react';
+import { useState} from 'react';
 import beeIcon from '../images/bee.png';
 import honeycomb from '../images/honey-comb.png';
 import miniHive from '../images/sweet.png';
+import ProgramForm from './ProgramForm';
 
 export default function ProgramInfo () {
+  let [isOpen, setIsOpen] = useState(false)
+ 
   return (
+    <>
+    { isOpen && <ProgramForm setIsOpen={setIsOpen} isOpen={isOpen} /> }
+
     <section className="pb-20">
       <div className=" text-xl lg:text-2xl font-black py-16 text-center">
         Programs
       </div>
-      <div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 w-full justify-center items-center lg:px-10 gap-5">
-          <div className="bg-yellow-500 lg:px-20 py-20 lg:py-40 lg:rounded-3xl font-bold text-2xl flex flex-col justify-center items-center">
-            <img src={beeIcon} alt="beeIcon" className="pb-2 w-40" />
-            <div>One-on-One Tutoring</div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-full justify-center items-center lg:px-10 gap-5">
+        <div className="bg-yellow-500 lg:px-20 py-20 lg:py-40 lg:rounded-3xl font-bold text-2xl flex flex-col justify-center items-center">
+          <img src={beeIcon} alt="beeIcon" className="pb-2 w-40" />
+          <div>One-on-One Tutoring</div>
+        </div>
+        <div className="bg-white p-10 lg:p-12 rounded-3xl">
+          <div className="font-bold text-left pb-5 px-5">
+            Stop stressing about “New Math”!
           </div>
-          <div className="bg-white p-10 lg:p-12 rounded-3xl">
-            <div className="font-bold text-left pb-5 px-5">
-              Stop stressing about “New Math”!
-            </div>
-            <div className='p-5'>
-              One on One Tutoring services are conveniently provided via
-              <strong>{" "}Instagram @_holisticedu.{" "}</strong> In person sessions will be available in
-              Atlanta coming soon. Does your child tend to forget things said
-              during math lessons easily? No problem! Tell me the standard/skill
-              or simply send a picture of what they are learning for the
-              week/unit, and I have it covered from there. No more frustrating
-              late nights at the table that end with you giving them the answers
-              (oh we know). Watch them master content and teach you a few new
-              strategies. Feel the reward of knowing you invested in your
-              child’s academic success! Have a certified educator mirror your
-              child’s <strong>IEP math goals and strategies</strong> as closely as possible. You
-              pick the time slots that work best for your schedule and see your
-              child’s overall level of confidence increase!
-            </div>
-            <div className="grid  grid-cols-1 lg:grid-rows-1 justify-evenly pt-5 gap-5">
-              <div className="font-black text-center px-7 lg:pl-7 lg:text-center">
-                Sign up for updates...<br></br> Enrollment starting soon!
-              </div>
-             {/*  <input
-                type="text"
-                placeholder="Email Address"
-                className="border-black border-2 border-solid p-5 rounded-3xl"
-              /> */}
-              <a href="mailto:123alleyesonmellc@gmail.com?subject=One%20on%20One%20Tutoring&body=Sign%20me%20up%20for%20updates" className="bg-black text-white font-bold p-5 rounded-3xl text-center mt-2 mx-9 lg:mx-5">Click to sign up!</a>
-             {/*  <button className="bg-black text-white font-bold p-5 rounded-3xl">
-                Sign Up
-              </button> */}
-            </div>
+          <div className="p-5">
+            One on One Tutoring services are conveniently provided via
+            <strong>{' '}Instagram @_holisticedu.{' '}</strong>
+            {' '}
+            In person sessions will be available in
+            Atlanta coming soon. Does your child tend to forget things said
+            during math lessons easily? No problem! Tell me the standard/skill
+            or simply send a picture of what they are learning for the
+            week/unit, and I have it covered from there. No more frustrating
+            late nights at the table that end with you giving them the answers
+            (oh we know). Watch them master content and teach you a few new
+            strategies. Feel the reward of knowing you invested in your
+            child’s academic success! Have a certified educator mirror your
+            child’s
+            {' '}
+            <strong>IEP math goals and strategies</strong>
+            {' '}
+            as closely as possible. You
+            pick the time slots that work best for your schedule and see your
+            child’s overall level of confidence increase!
+          </div>
+          <div className="grid grid-cols-1 w-full justify-center items-center ">
+          <div className='font-bold text-center pb-5'>Sign up for updates below! Enrollment will begin soon!</div>
+      
+          <div><button type="submit" onClick={() => setIsOpen(true)}className='bg-black text-white w-full rounded-3xl py-5'>Sign Up</button></div>
+
           </div>
         </div>
+
       </div>
+
       <div>
         <div className="grid grid-cols-1 lg:grid-cols-2 w-full justify-center items-center lg:px-10 gap-5">
           <div className="bg-pink-400 lg:px-20 py-20 lg:py-40 lg:rounded-3xl font-bold text-2xl flex flex-col justify-center items-center">
@@ -58,33 +63,35 @@ export default function ProgramInfo () {
             <div>Parent Bootcamp</div>
           </div>
           <div className="bg-white p-10 lg:p-12 rounded-3xl">
-          <div className="font-bold text-left pb-5 px-5">
-          Have you ever looked at your child’s MATH homework and
+            <div className="font-bold text-left pb-5 px-5">
+              Have you ever looked at your child’s MATH homework and
               automatically got a headache?{' '}
             </div>
-            <div className='p-5'>
+            <div className="p-5">
               It looks like a foreign language, right? Did you help them find
               the answer and it came back marked wrong? Children today are being
               taught "NEW" strategies to solve math problems. As an educator,
               I’ve never felt comfortable sending work home for parents to
-              review/assist you
-            </div>
-            <div className="grid  grid-cols-1 lg:grid-rows-1 justify-evenly pt-5 gap-5">
-              <div className="font-black text-center">
-                Sign up for updates below ...<br></br> Enrollment starting soon!
-              </div>
-           {/*    <input
-                type="text"
-                placeholder="Email Address"
-                className="border-black border-2 border-solid p-5 rounded-3xl"
-              />
-              <button className="bg-black text-white font-bold p-5 rounded-3xl">
-                Sign Up
-              </button> */}
-          <a href="mailto:123alleyesonmellc@gmail.com?subject=Parent%20Bootcamp&body=Sign%20me%20up%20for%20updates" className="bg-black text-white font-bold p-5 rounded-3xl text-center mt-2 mx-9 lg:mx-5">Click to sign up!</a>
+              review/assist your child without offering the opportunities to 
+              teach parents the new strategies. How can you adequately help 
+              your child if you have no clue what they are doing? You cannot. 
+              Parent Boot Camp allows me to go through step by step and model 
+              how to accurately use the "NEW MATH" strategies being used in 
+              classrooms around the world today. This opportunity will ensure 
+              you are familiar with what your child will be bringing home. 
+              Attending our parents only, judgement free boot camp will give 
+              you the confidence needed to fully support your young scholar. 
+              This is NOT the same math we did in school. Remember, it takes a village! 
+              Let me help you so you can help your baby.
 
             </div>
+            <div className="grid grid-cols-1 w-full justify-center items-center ">
+          <div className='font-bold text-center pb-5'>Sign up for updates below! Enrollment will begin soon!</div>
+      
+          <div><button type="submit" onClick={() => setIsOpen(true)}className='bg-black text-white w-full rounded-3xl py-5'>Sign Up</button></div>
+
           </div>
+                    </div>
         </div>
       </div>
       <div>
@@ -100,28 +107,18 @@ export default function ProgramInfo () {
               10AM-12PM throughout the year for parents and children. You must
               stay with your child the entire duration of the lesson.{' '}
             </div>
-            <div className="grid  grid-cols-1 lg:grid-rows-1 justify-evenly pt-5 gap-5">
-              <div className="font-black text-center lg:text-center">
-                Sign up for updates below ... Enrollment starting soon!
-              </div>
-              {/* <input
-                type="text"
-                placeholder="Email Address"
-                className="border-black border-2 border-solid p-5 rounded-3xl"
-              />
-              <button className="bg-black text-white font-bold p-5 rounded-3xl">
-                Sign Up
+            <div className="grid grid-cols-1 w-full justify-center items-center ">
+          <div className='font-bold text-center pb-5'>Sign up for updates below! Enrollment will begin soon!</div>
+      
+          <div><button type="submit" onClick={() => setIsOpen(true)}className='bg-black text-white w-full rounded-3xl py-5'>Sign Up</button></div>
 
-              </button> */}
-            <a href="mailto:123alleyesonmellc@gmail.com?subject=Mathematics%20In%20The%20Park&body=Sign%20me%20up%20for%20updates" className="bg-black text-white font-bold p-5 rounded-3xl text-center mt-2 mx-9 lg:mx-5">Click to sign up!</a>
-
-            </div>
+          </div>
           </div>
         </div>
       </div>
       <div>
         <div className="grid grid-cols-1 lg:grid-cols-2 w-full justify-center items-center lg:px-10 gap-5">
-          <div className='pt-20'>
+          <div className="pt-20">
             <div className="bg-purple-500 lg:px-20 py-20 lg:py-40 lg:rounded-3xl font-bold text-2xl flex flex-col justify-center items-center">
               <img src={miniHive} alt="beeIcon" className=" w-40" />
               <div>Mathematician Toolbox</div>
@@ -130,7 +127,7 @@ export default function ProgramInfo () {
             <div className="border-black border-solid lg:rounded-3xl border-2 p-5 lg:mt-5 bg-black text-white">
               <div className="font-bold lg:pt-5 pb-5 pl-2">
                 Have your child’s math classroom delivered to your front door!
-          
+
               </div>
 
               <div className="pl-2">
@@ -169,24 +166,16 @@ export default function ProgramInfo () {
                 </strong>
               </div>
             </div>
-            <div className="grid  grid-cols-1 lg:grid-rows-1 justify-evenly pt-5 gap-5">
-              <div className="font-black text-center lg:text-center pt-14">
-                Sign up for updates below ... Enrollment starting soon!
-              </div>
-             {/*  <input
-                type="text"
-                placeholder="Email Address"
-                className="border-black border-2 border-solid p-5 rounded-3xl"
-              />
-              <button className="bg-black text-white font-bold p-5 rounded-3xl">
-                Sign Up
-              </button> */}
-          <a href="mailto:123alleyesonmellc@gmail.com?subject=Mathematician%20Toolbox&body=Sign%20me%20up%20for%20updates" className="bg-black text-white font-bold p-5 rounded-3xl text-center mt-2 mx-9 lg:mx-5">Click to sign up!</a>
+            <div className="grid grid-cols-1 w-full justify-center items-center ">
+          <div className='font-bold text-center pb-5'>Sign up for updates below! Enrollment will begin soon!</div>
+      
+          <div><button type="submit" onClick={() => setIsOpen(true)}className='bg-black text-white w-full rounded-3xl py-5'>Sign Up</button></div>
 
-            </div>
+          </div>
           </div>
         </div>
       </div>
     </section>
+    </>
   );
 }
