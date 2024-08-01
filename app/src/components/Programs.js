c27ccacab7ff55be563c0d1e761bcd09e687b2fd
sbcodes90 <sydneycburrell29@gmail.com>
2024-08-01 07:19:36 -0400

    update

--- app/src/components/Programs.js
+++ app/src/components/Programs.js
@@ -11,9 +11,9 @@ export default function Programs() {
     <div className="bg-yellow-100 text-center p-20 lg:px-40 lg:py-40">
       <div className=" text-xl lg:text-3xl font-black pb-20">Programs</div>
       <div className="grid lg:grid-cols-4 gap-5 justify-center	">
-        <Link to="home/program-info" className="bg-yellow-500 hover:bg-teal-200 hover:border-[10px] hover:border-black p-20 rounded-3xl font-bold text-2xl">
+        <div className="bg-yellow-500 hover:bg-teal-200 hover:border-[10px] hover:border-black p-20 rounded-3xl font-bold text-2xl">
           <div><img src={beeIcon} alt="beeIcon" className="pb-2" /></div>
-          One-on-One Tutoring</Link>
+          One-on-One Tutoring</div>
         <div className="bg-yellow-500 hover:bg-teal-200 hover:border-[10px] hover:border-black p-20 rounded-3xl font-bold text-2xl">
           <div><img src={honeycomb} alt="beeIcon" className="pb-2" /></div>
           Parent Bootcamp</div>
